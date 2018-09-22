5cc19a1f20af465716b4dafd64dad6e3bee8f25a
574549756 <574549756@qq.com>
2018-09-23 00:12:51 +0800

    新建时清空数据

--- src/js/music-list.js
+++ src/js/music-list.js
@@ -54,6 +54,7 @@
             this.model = model
             this.view.render(this.model.data)
             this.bindEvents()
+            this.bindEventHub()
             this.getAllSongs()
         },
         getAllSongs() {
@@ -84,6 +85,9 @@
             window.eventHub.on('create', songData => {
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
+            })
+            window.eventHub.on('new', () => {
+                this.view.clearActive()
             })
         }
     }
