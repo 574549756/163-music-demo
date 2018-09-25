fb653a47cd480b256678935cbad8364edac4cb3d
574549756 <574549756@qq.com>
2018-09-26 04:04:00 +0800

    page-1-1子模块

--- src/js/index/page-1.js
+++ src/js/index/page-1.js
@@ -18,6 +18,8 @@
             this.view.init()
             this.model = model
             this.bindEventHub()
+            this.loadModel1()
+            this.loadModel2()
         },
         bindEventHub() {
             window.eventHub.on('selectTab', tabName => {
@@ -27,6 +29,22 @@
                     this.view.hide()
                 }
             })
+        },
+        loadModel1() {
+            let script1 = document.createElement('script')
+            script1.src = './js/index/page-1-1.js' //相对于htmls
+            script1.onload = function() {
+                console.log('模块１加载完毕')
+            }
+            document.body.appendChild(script1)
+        },
+        loadModel2() {
+            let script2 = document.createElement('script')
+            script2.src = './js/index/page-1-2.js' //相对于htmls
+            script2.onload = function() {
+                console.log('模块２加载完毕')
+            }
+            document.body.appendChild(script2)
         }
     }
     controller.init(view, model)
