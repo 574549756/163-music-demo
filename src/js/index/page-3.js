19071724560c8d9d562a188706107cffc71e9dd1
574549756 <574549756@qq.com>
2018-10-05 05:08:39 +0800

    函数节流

--- src/js/index/page-3.js
+++ src/js/index/page-3.js
@@ -14,8 +14,11 @@
     let model = {
         data: [],
         search: name => {
-            var query = new AV.Query('Song')
-            query.contains('name', name)
+            var query1 = new AV.Query('Song')
+            query1.contains('name', name)
+            var query2 = new AV.Query('Song')
+            query2.contains('artist', name)
+            var query = AV.Query.or(query1, query2)
             query.find().then(function(results) {
                 $('#searchResult').empty()
                 if (results.length === 0) {
@@ -23,10 +26,13 @@
                 } else {
                     for (var i = 0; i < results.length; i++) {
                         let song = results[i]
-                        let li = `<li data-id="${song.id}">${
+                        let li = `
+                        <li data-id="${song.id}">
+                            <a href="./song.html?id=${song.id}">${
                             song.attributes.name
                         } - ${song.attributes.artist}
-                            </li>`
+                        </a>
+                        </li>`
                         $('#searchResult').append(li)
                     }
                 }
@@ -48,13 +54,21 @@
                     this.view.hide()
                 }
             })
+            let timer = null
             $('input#search').on('input', e => {
-                let $input = $(e.currentTarget)
-                let value = $input.val().trim()
-                if (value === '') {
-                    return
+                if (timer) {
+                    window.clearTimeout(timer)
                 }
-                this.model.search(value)
+                timer = setTimeout(() => {
+                    console.log('时间到')
+                    timer = null
+                    let $input = $(e.currentTarget)
+                    let value = $input.val().trim()
+                    if (value === '') {
+                        return
+                    }
+                    this.model.search(value)
+                }, 400)
             })
         }
     }
