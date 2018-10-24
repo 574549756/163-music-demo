32d5de25cdd27a09da0210ca5dba535e64775736
Oracle <574549756@qq.com>
2018-10-24 23:58:12 +0800

    transport data to playlist

--- src/js/playlist/loadMusic.js
+++ src/js/playlist/loadMusic.js
@@ -1,10 +1,21 @@
 {
     let view = {}
-    let model = {}
+    let model = {
+        data:{
+            songs:[]
+        }
+    }
     let controller = {
         init(){
             this.view = view
             this.model = model
+            this.bindEventHub()
+        },
+        bindEventHub(){
+            window.eventHub.on('getSongs',songs=>{
+                this.model.data.songs = songs
+                console.log(this.model.data)
+            })
         }
     }
     controller.init(view,model)
--- src/js/playlist/loadPlaylist.js
+++ src/js/playlist/loadPlaylist.js
@@ -5,12 +5,31 @@
     let model = {
         data: {
             playlist: []
+        },
+        getPlaylist(playlists) {
+            var query = new AV.Query('Playlist');
+            query.get(playlists).then(function (playlist) {
+                Object.assign(this.data.playlist, {
+                    id: playlist.id,
+                    ...playlist.attributes
+                })
+                return playlist
+            });
         }
     }
     let controller = {
         init() {
             this.view = view
             this.model = model
+            
+            this.bindEventHub()
+        },
+        bindEventHub() {
+            window.eventHub.on('getPlaylist', playlist => {
+                this.model.getPlaylist(playlist).then(()=>{
+                    this.model.render(this.model.data)
+                })
+            })
         }
     }
     controller.init(view, model)
