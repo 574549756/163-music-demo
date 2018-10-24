d4c65bebfda13d60e1cd971b8384f44d80679fd1
Oracle <574549756@qq.com>
2018-10-24 23:36:09 +0800

    get songBeyoundSonglist

--- src/js/playlist/app.js
+++ src/js/playlist/app.js
@@ -1,21 +1,64 @@
 {
     let view = {}
     let model = {
-        data:{
-            playlist:[],
-            songs:[]
+        data: {
+            playlist: undefined,
+            songs: [],
+            templateMap:[]
+        },
+        get(playlistId) {
+            this.model.data.playlist = playlistId
+            let playlistResult = new AV.Object.createWithoutData(
+                'Playlist',
+                playlistId
+            )
+            var query = new AV.Query('playlistMap')
+            query.equalTo('playlistPointer', playlistResult)
+            return query
+                .find()
+                .then(playlistMap => {
+                    let songResultsId = []
+                    playlistMap.forEach(scm => {
+                        this.data.templateMap.push(scm.id)
+                        var songs = scm.get('songPointer')
+                        songResultsId.push(songs.id)
+                    })
+                    return songResultsId
+                })
+                .then(async songResultsId => {
+                    let songAfter = []
+                    var searchSong = new AV.Query('Song')
+                    for (i = 0; i < songResultsId.length; i++) {
+                        await searchSong.get(songResultsId[i]).then(songResult => {
+                            songAfter.push(songResult)
+                        })
+                    }
+                    return songAfter
+                })
+                .then(songAfter => {
+                    this.data.songs = songAfter.map(song => {
+                        return {
+                            id: song.id,
+                            ...song.attributes
+                        }
+                    })
+                })
         }
     }
     let controller = {
-        init(view,model){
+        init(view, model) {
             this.view = view
             this.model = model
             let id = this.getPlaylistId()
+            this.model.get(id).then(() => {
+                window.eventHub.emit('getPlaylist', this.model.data.playlist)
+                window.eventHub.emit('getSongs', this.model.data.songs)
+            })
             this.bindEventHub()
             this.loadPlaylist()
             this.loadMusic()
         },
-        bindEventHub(){
+        bindEventHub() {
         },
         getPlaylistId() {
             let search = window.location.search
@@ -37,18 +80,18 @@
             }
             return id
         },
-        loadPlaylist(){
+        loadPlaylist() {
             let script1 = document.createElement('script')
             script1.src = './js/playlist/loadPlaylist.js'
-            script1.onload = function(){}
+            script1.onload = function () { }
             document.body.appendChild(script1)
         },
-        loadMusic(){
+        loadMusic() {
             let script2 = document.createElement('script')
             script2.src = './js/playlist/loadMusic.js'
-            script2.onload = function(){}
+            script2.onload = function () { }
             document.body.appendChild(script2)
         }
     }
-    controller.inid(view,model)
+    controller.inid(view, model)
 }
