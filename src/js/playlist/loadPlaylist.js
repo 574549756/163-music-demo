3cb2376d34b2b76f18fcfbc97cfaa2a9e89eba3b
Oracle <574549756@qq.com>
2018-10-26 03:11:18 +0800

    完成数据更新

--- src/js/playlist/loadPlaylist.js
+++ src/js/playlist/loadPlaylist.js
@@ -1,18 +1,40 @@
 {
     let view = {
-        el: '',
-        render(Playlist){
-            $('title').text(`${Playlist.name}`)
+        el: 'header',
+        template: `
+        <section class="cover">
+            <div class="coverImg"></div>
+            <div class="playlist-title">{{playlist.name}}</div>
+        </section>
+        <section class="summary">
+            <div class="summaryContent">简介：{{playlist.summary}}</div>
+            <div class="buttonMore"></div>
+        </section>
+        `,
+        init() {
+            this.$el = $(this.el)
+        },
+        render(data) {
+            let { playlists } = data
+            let $li = $(
+                this.template
+                    .replace('{{playlist.name}}', playlists.name)
+                    .replace('{{playlist.summary}}', playlists.summary)
+            )
+            console.log($li)
+            $li.find('.cover').css('background', `url(${playlists.url})`)
+            $li.find('.coverImg').css('background', `url(${playlists.url})`)
+            this.$el.find('.playlistArea').append($li)
         }
     }
     let model = {
         data: {
-            playlist: []
+            playlists: []
         },
         getPlaylist(playlists) {
             var query = new AV.Query('Playlist');
-            return query.get(playlists).then( (playlist)=> {
-                Object.assign(this.data.playlist, {
+            return query.get(playlists).then((playlist) => {
+                Object.assign(this.data.playlists, {
                     id: playlist.id,
                     ...playlist.attributes
                 })
@@ -23,18 +45,19 @@
     let controller = {
         init() {
             this.view = view
+            this.view.init()
             this.model = model
-            
+
             this.bindEventHub()
         },
-        changeTitle(){
-            
+        changeTitle() {
+
         },
         bindEventHub() {
             window.eventHub.on('getPlaylist', playlist => {
-                this.model.getPlaylist(playlist).then(()=>{
-                    this.view.render(this.model.data.playlist)
-                    
+                this.model.getPlaylist(playlist).then(() => {
+                    this.view.render(this.model.data)
+
                 })
             })
         }
